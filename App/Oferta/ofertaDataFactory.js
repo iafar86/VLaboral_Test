2a2cbbff143643769825c4dcdf5ab9ac52e2a8e6
franpaz <franfpaz@gmail.com>
2015-11-25 16:09:00 -0300

    ofertaDataFactory.js con las llamadas nuevas

--- App/Oferta/ofertaDataFactory.js
+++ App/Oferta/ofertaDataFactory.js
@@ -1,10 +1,10 @@
-﻿vLaboralApp.factory('ofertaDataFactory', function ($resource) {
-    var urlApi = "http://localhost:32069";
-    return $resource(urlApi + 'api/Ofertas/:id',
-           { id: '@id' },
-           { 'update': { method: 'PUT' } }
-        );
-});
+﻿//vLaboralApp.factory('ofertaDataFactory', function ($resource) {
+//    var urlApi = "http://localhost:32069";
+//    return $resource(urlApi + 'api/Ofertas/:id',
+//           { id: '@id' },
+//           { 'update': { method: 'PUT' } }
+//        );
+//});
 
 vLaboralApp.factory('ofertaDataFactory', function ($http) {
 
